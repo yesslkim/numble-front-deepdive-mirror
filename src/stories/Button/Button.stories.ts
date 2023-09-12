4dd7036336a1ca3baa53831977ced66cfec69aaa
Ye Seul Kim <yskim940914@gmail.com>
2023-09-12 21:35:20 +0900

    Feat: 스토리북 추가

--- src/stories/Button/Button.stories.ts
+++ src/stories/Button/Button.stories.ts
@@ -2,51 +2,32 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { Button } from './Button';
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: 'Example/Button',
   component: Button,
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'centered'
-  },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: {
-      control: 'color'
-    }
-  }
+  tags: ['autodocs']
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary: Story = {
+export const Filled: Story = {
   args: {
-    primary: true,
+    variant: 'filled',
     label: 'Button'
   }
 };
 
-export const Secondary: Story = {
+export const Outlined: Story = {
   args: {
+    variant: 'outlined',
     label: 'Button'
   }
 };
 
-export const Large: Story = {
+export const Text: Story = {
   args: {
-    size: 'large',
-    label: 'Button'
-  }
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
+    variant: 'text',
     label: 'Button'
   }
 };
--- src/stories/Button/Button.tsx
+++ src/stories/Button/Button.tsx
@@ -1,57 +1,114 @@
 /* eslint-disable react/no-unknown-property */
-import React from 'react';
 import './button.css';
 
+type Variant = 'filled' | 'outlined' | 'text';
+type Color =
+  | 'success'
+  | 'error'
+  | 'blue'
+  | 'red'
+  | 'green'
+  | 'grey'
+  | 'black'
+  | 'white';
+
 interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
+  variant?: Variant;
+  color?: Color;
+  size?: 'small' | 'large' | 'xlarge';
+  fullWidth?: boolean;
+  radius?: 4 | 8 | 12 | 16;
   label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
+  startIcon?: 'IconUser' | 'IconHome';
+  endIcon?: 'IconUser' | 'IconHome';
 }
 
-/**
- * Primary UI component for user interaction
- */
+const brandColor = {
+  success: '#3068e9',
+  error: '#e02525',
+  blue: '#0d24cf',
+  red: '#b80b0b',
+  green: '#31d022',
+  grey: '#787878',
+  black: '#1e1e1e',
+  white: '#ffffff'
+};
+
+const getColorStyles = (color?: Color) => {
+  let mainColor;
+  let subColor;
+
+  switch (color) {
+    case 'white':
+    case 'green':
+    case 'success':
+      mainColor = brandColor[color];
+      subColor = brandColor.black;
+      break;
+    case 'black':
+    case 'grey':
+    case 'red':
+    case 'blue':
+    case 'error':
+      mainColor = brandColor[color];
+      subColor = brandColor.white;
+      break;
+    default:
+      mainColor = brandColor.black;
+      subColor = brandColor.white;
+  }
+
+  return {
+    mainColor,
+    subColor
+  };
+};
+
+const getThemeStyle = (theme: Variant, mainColor: string, subColor: string) => {
+  if (!theme || theme === 'filled') {
+    return {
+      backgroundColor: `${mainColor}`,
+      color: `${subColor}`
+    };
+  }
+
+  if (theme === 'outlined') {
+    return {
+      border: `1px solid ${mainColor}`,
+      color: `${mainColor}`
+    };
+  }
+
+  if (theme === 'text') {
+    return {
+      backgroundColor: 'transparent',
+      color: `${mainColor}`
+    };
+  }
+};
+
 export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
+  variant = 'filled',
+  size = 'large',
+  color = 'black',
+  label = 'Button',
+  fullWidth = false,
+  radius = 4,
+  startIcon,
+  endIcon
 }: ButtonProps) => {
-  const mode = primary
-    ? 'storybook-button--primary'
-    : 'storybook-button--secondary';
+  const { mainColor, subColor } = getColorStyles(color);
+  const themeStyle = getThemeStyle(variant, mainColor, subColor);
+  const width = fullWidth ? 'btn-fullWidth' : '';
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(
-        ' '
-      )}
-      {...props}
+      style={themeStyle}
+      className={[`btn-size-${size}`, width, `btn-radius-${radius}`].join(' ')}
     >
+      {startIcon && <i className={`icon-start ${startIcon}`}></i>}
       {label}
-      <style jsx>{`
-        button {
-          background-color: ${backgroundColor};
-        }
-      `}</style>
+      {endIcon && <i className={`icon-end ${endIcon}`}></i>}
     </button>
   );
 };
